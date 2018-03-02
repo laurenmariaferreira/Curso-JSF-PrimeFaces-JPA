d31de8f302bece8e51e286e344e067d87ab3e397
150017 <150017@A2909.labinfo.facens.br>
2018-03-01 22:43:24 -0300

    Cadastro do professor incompleto

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,37 @@
+package sistema.modelos;
+
+
+
+public class Professor {
+
+
+	private int registro;
+	private String nome;
+	private String especialidade;
+	private float salario;
+	
+	public int getRegistro() {
+		return registro;
+	}
+	public void setRegistro(int registro) {
+		this.registro = registro;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEspecialidade() {
+		return especialidade;
+	}
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
+	public float getSalario() {
+		return salario;
+	}
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+}
